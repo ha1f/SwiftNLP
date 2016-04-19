f52637773034cae5ac1911ff840bc337141bae6d
ha1f <half.ldk.hiz@gmail.com>
2016-04-19 17:36:47 +0900

    Initial Commit

---
+++ nlp100/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nlp100
+//
+//  Created by はるふ on 2016/04/19.
+//  Copyright © 2016年 はるふ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
